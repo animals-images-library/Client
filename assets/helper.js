be710d26e567061ebbf9d794286b04d449ad3307
dayuhidayatullah <dayuhidayat123@gmail.com>
2021-01-08 03:02:04 +0700

    add-template/jquery/css-login-page-and-register

---
+++ assets/helper.js
@@ -0,0 +1,93 @@
+const url = 'http://localhost:3000'
+
+function showMainPage() {
+  $('#navbar').show()
+  $('#login-page').hide()
+  $('#register-page').hide()
+  $('#list-animals').show()
+  // $('#btn-logout').show()
+}
+function showLoginPage() {
+  $('#navbar').hide()
+  $('#login-page').show()
+  $('#register-page').hide()
+  $('#list-animals').hide()
+  // $('#btn-logout').hide()
+}
+function showRegisterPage(){
+  $('#navbar').hide()
+  $('#login-page').hide()
+  $('#register-page').show()
+  $('#list-animals').hide()
+}
+function register () {
+  const email =  $('#email-regis').val()
+  const password =  $('#password-regis').val()
+  $.ajax({
+      method: 'POST',
+      url: `${url}/register`,
+      data: {
+          email,
+          password
+      }
+  })
+    .done(response => {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: `Register ${response.email} Success`,
+      })
+        console.log(response)
+        showLoginPage()
+    })
+    .fail(xhr => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${xhr.responseJSON.message}`,
+      })
+        console.log(xhr)
+    })
+
+}
+
+function login () {
+  const email = $('#email-login').val()
+  const password = $('#password-login').val()
+  
+  $.ajax({
+    method: 'POST',
+    url: `${url}/login`,
+    data: {
+      email,
+      password
+    }
+  })
+    .done(response => {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        text: 'Login succes'
+      })
+      localStorage.setItem('access_token', response.access_token)
+      showMainPage()
+    })
+    .fail(xhr => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${xhr.responseJSON.message}`,
+      })
+    })
+}
+
+function logout(){ 
+  localStorage.clear()
+  Swal.fire({
+    icon: 'success',
+    text: 'Logout Success',
+    showConfirmButton: false,
+    timer: 1500
+  })
+  showLoginPage()
+}
